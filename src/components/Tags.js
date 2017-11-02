93adf5e09e222c1713037e45e8a12e6addc22c54
Peter Destasio <>
2017-11-02 14:09:01 -0400

    added animations to remove tag

--- src/components/Tags.js
+++ src/components/Tags.js
@@ -1,6 +1,7 @@
 import React, { PureComponent } from 'react';
 import {
   StyleSheet,
+  LayoutAnimation,
   View
 } from 'react-native';
 import TagsArea from './TagsArea';
@@ -9,6 +10,9 @@ import type { TagObject } from '../types';
 type Props = {
   // Array of tag titles
   tags: string[],
+
+  // tag swapping animation duration in ms
+  animationDuration: number,
   // Passes onPressAddNewTag callback down to TagsArea component
   onPressAddNewTag: () => void,
 };
@@ -21,12 +25,38 @@ export default class Tags extends PureComponent {
 
   props: Props;
 
+  static defaultProps = {
+      animationDuration: 250
+  };
+
   state: State = {
     // Convert passed array of tag titles to array of objects of TagObject type,
     // so ['tag', 'another'] becomes [{ title: 'tag' }, { title: 'another' }]
     tags: [...new Set(this.props.tags)]       // remove duplicates
       .map((title: string) => ({ title })),   // convert to objects
   };
+
+  // Animate layout changes when dragging or removing a t ag
+  componentWillUpdate() {
+      LayoutAnimation.configureNext({
+          ...LayoutAnimation.Presets.easeInEaseOut,
+          duration: this.props.animationDuration
+      });
+  }
+
+  // remove tags
+  removeTag = (tag: TagObject): void => {
+      this.setState((state: State) =>{
+          const index = state.tags.findIndex(({ title })=> title === tag.title);
+          return {
+              tags: [
+                  // remove the tag
+                  ...state.tags.slice(0, index),
+                  ...state.tags.slice(index + 1),
+              ]
+          }
+      });
+  }
 
   render() {
     const { tags } = this.state;
@@ -37,7 +67,7 @@ export default class Tags extends PureComponent {
 
         <TagsArea
           tags={tags}
-          onPress={() => {}} // do nothing for now
+          onPress={this.removeTag} // do nothing for now
           onRenderTag={() => {}} // do nothing for now
           onPressAddNew={this.props.onPressAddNewTag}
         />
